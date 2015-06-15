a800804b0e911d4459034e62c54bd2c3150c50e0
carlogeertse <carlogeertse@hotmail.com>
2015-06-15 15:09:14 +0200

    Connect gemaakt en adden tentamens

--- tentInvoeren.php
+++ tentInvoeren.php
@@ -18,5 +18,22 @@
         <h3>
             Invoeren van tentamens op het rooster. Hier worden alle tentamens ingeroosterd.
         </h3>
+
+        <?php include("connectDB.php"); ?>
+        <form action="action_page.php">
+            First name:<br>
+            <select name="cars">
+                <option value="volvo">Volvo XC90</option>
+                <option value="saab">Saab 95</option>
+                <option value="mercedes">Mercedes SLK</option>
+                <option value="audi">Audi TT</option>
+            </select>
+            <input type="submit" value="Submit">
+            <br>
+            Last name:<br>
+            <input type="text" name="lastname" value="Mouse">
+            <br><br>
+            <input type="submit" value="Submit">
+        </form>
     </body>
 </html>
