d3b13d852531b7abe78bf3a45c62935c26ef7b53
Tobesitas <tdejager@telfort.nl>
2015-06-15 12:08:27 +0200

    je moeder is opzich best wel mooi, althans zij zei dat gisteravond

---
+++ Home.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>
+            Homepage
+        </title>
+        <link rel = "stylesheet" type = "text/css" href = "Style.css">
+    </head>
+
+    <body>
+            <div class = "menu">
+                <div class = "inner">
+                    <ul>
+                        <li>
+                            <a href = "#">Home</a>
+                        </li>
+                        <li>
+                            <a href = "#">Rooster</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            
+            <h1>
+                <b>
+                    Homepage TWZ
+                </b>
+            </h1>
+    </body>
+</html>
