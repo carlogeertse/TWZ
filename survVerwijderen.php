9ccf90f3de8ba6b718e9ce735c93e27c22b385bc
Tobesitas <tdejager@telfort.nl>
2015-06-16 13:17:21 +0200

    PUSH!!!! PUUUUUUUUSH!!!!!!!! It's a boy!!!!

--- survVerwijderen.php
+++ survVerwijderen.php
@@ -17,16 +17,33 @@
             Welke Surveillant wilt u verwijderen?
         </h1>
 
-        <select name = "surveillanten">
-            <?php
-                $sql = "SELECT Achternaam, Voornaam FROM surveillanten ORDER BY Achternaam;";
-                $sql = $conn->query($sql);
-                if ($sql-> num_rows > 0) {
-                    while ($row = $sql->fetch_assoc()) {
-                        echo "<option>" . $row['Achternaam'] . ", " . $row['Voornaam'] . "</option>";
-                    }
-                }
-            ?>
-        </select>
+        <?php
+            if($_SERVER["REQUEST_METHOD"] == "POST") {
+                $sqlDelete = "DELETE FROM surveillanten WHERE pers_nummer = '" . $_POST['surveillanten'] . "';";
+                $sqlDelete = $conn->query($sqlDelete);
+            }
+        ?>
+        <form action = "survVerwijderen.php" method = "post">
+            <table>
+                <tr>
+                    <td>
+                        <select name = "surveillanten">
+                            <?php
+                                $sql = "SELECT Achternaam, Voornaam, pers_nummer FROM surveillanten ORDER BY Achternaam;";
+                                $sql = $conn->query($sql);
+                                if ($sql-> num_rows > 0) {
+                                    while ($row = $sql->fetch_assoc()) {
+                                        echo '<option value = ' . $row['pers_nummer'] . '>' . $row['Achternaam'] . ', ' . $row['Voornaam'] . '</option>';
+                                    }
+                                }
+                            ?>
+                        </select>
+                    </td>
+                    <td>
+                        <input type = "submit" value = "Verwijder Surveillant">
+                    </td>
+                </tr>
+            </table>
+        </form>
     </body>
 </html>
