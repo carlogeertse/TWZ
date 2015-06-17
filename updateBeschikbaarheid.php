0db1de1170c09d834aea2d6074821d1f274fdd8c
carlogeertse <carlogeertse@hotmail.com>
2015-06-17 11:33:29 +0200

    Javascript voor checkboxes werkt nu half

---
+++ updateBeschikbaarheid.php
@@ -0,0 +1,12 @@
+<?php
+    //Connect met de database
+    include("connectDB.php");
+
+    //Ophalen Post variabelen
+    $isChecked = $_POST['isChecked'];
+    $uid = $_POST['uid'];
+    $rank = $_POST['rank'];
+
+    echo 'success';
+
+?>
