335fa4f4ebada076942c3d7e595980e43a696e65
Tobesitas <tdejager@telfort.nl>
2015-06-17 16:26:24 +0200

    Commit and Push they said... It will be fun they said...

--- test.php
+++ test.php
@@ -1,56 +1,49 @@
-<!DOCTYPE html>
-<html lang="en">
+<script>
+    var nowTemp = new Date();
+    var now = new Date(nowTemp.getFullYear(), nowTemp.getMonth(), nowTemp.getDate(), 0, 0, 0, 0);
+
+    var checkin = $('#dpd1').datepicker({
+        onRender: function(date) {
+            return date.valueOf() < now.valueOf() ? 'disabled' : '';
+        }
+    }).on('changeDate', function(ev) {
+        if (ev.date.valueOf() > checkout.date.valueOf()) {
+            var newDate = new Date(ev.date)
+            newDate.setDate(newDate.getDate() + 1);
+            checkout.setValue(newDate);
+        }
+        checkin.hide();
+        $('#dpd2')[0].focus();
+    }).data('datepicker');
+    var checkout = $('#dpd2').datepicker({
+        onRender: function(date) {
+            return date.valueOf() <= checkin.date.valueOf() ? 'disabled' : '';
+        }
+    }).on('changeDate', function(ev) {
+        checkout.hide();
+    }).data('datepicker');
+    $('.datepicker').datepicker('show');
+</script>
+<html>
 <head>
-    <meta charset="utf-8">
-    <title>Show Hide Dropdown Using CSS</title>
-    <style type="text/css">
-        ul{
-            padding: 0;
-            list-style: none;
-        }
-        ul li{
-            width: 100px;
-            display: inline-block;
-            position: relative;
-            text-align: center;
-            line-height: 21px;
-        }
-        ul li a{
-            display: block;
-            padding: 5px 10px;
-            color: #333;
-            background: #f2f2f2;
-            text-decoration: none;
-        }
-        ul li a:hover{
-            color: #fff;
-            background: #939393;
-        }
-        ul li ul{
-            display: none;
-            position: absolute;
-            z-index: 999;
-            left: 0;
-        }
-        ul li:hover ul{
-            display: block; /* display the dropdown */
-        }
-    </style>
+    <title>
+
+    </title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+    <script src="js/jquery-2.1.4.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
 </head>
+
 <body>
-<ul>
-    <li><a href="#">Home</a></li>
-    <li><a href="#">About</a></li>
-    <li>
-        <a href="#">Products &#9662;</a>
-        <ul>
-            <li><a href="#">Laptops</a></li>
-            <li><a href="#">Monitors</a></li>
-            <li><a href="#">Printers</a></li>
-        </ul>
-    </li>
-    <li><a href="#">Contact</a></li>
-</ul>
-<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eu sem tempor, varius quam at, luctus dui. Mauris magna metus, dapibus nec turpis vel, semper malesuada ante. Vestibulum id metus ac nisl bibendum scelerisque non non purus. Suspendisse varius nibh non aliquet sagittis. In tincidunt orci sit amet elementum vestibulum. Vivamus fermentum in arcu in aliquam. Quisque aliquam porta odio in fringilla. Vivamus nisl leo, blandit at bibendum eu, tristique eget risus. Integer aliquet quam ut elit suscipit, id interdum neque porttitor. Integer faucibus ligula.</p>
+    <?php include("Menu.php"); ?>
+    <div class = "input-attend date" id = "dp3" data-date = "12-02-2012" data-date-format = "dd-mm-yyyy">
+        <input class = "span2" size = "16" type = "text" value = "12-02-2012">
+        <span class = "add-on">
+            <i class = "icon-th">
+
+            </i>
+        </span>
+    </div>
 </body>
-</html>                                		
+</html>
