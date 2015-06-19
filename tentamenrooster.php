bd9782bff18d8ccee7d13086d58025f5d9f8509d
carlogeertse <carlogeertse@hotmail.com>
2015-06-19 09:21:19 +0200

    tentamenrooster eerste deel

--- tentamenrooster.php
+++ tentamenrooster.php
@@ -9,7 +9,13 @@
     </head>
 
     <body style = "background-image: url(src/download.jpg); position: relative; background-size: cover;">
-        <?php include("Menu.php"); ?>
+        <?php include("Menu.php");
+        if($_SERVER['REQUEST_METHOD'] == "POST")
+        {
+            echo "hoi";
+        }
+
+        ?>
         <h1>
             Hier ziet u de huidig beschikbare roosters:
         </h1>
@@ -24,14 +30,43 @@
                     AND T.datum BETWEEN'".date("Y-m-d", strtotime($jaar."W".$week."1"))."' AND '".date('Y-m-d', strtotime($jaar."W".$week."5"))."'";
             $result = $conn->query($sql);
 
+            $surveillantensql = "SELECT Voornaam, Tussenvoegsel, Achternaam, pers_nummer
+                                 FROM   surveillanten;";
+
+            $surveillantenresult = $conn->query($surveillantensql);
+
             if($result->num_rows > 0){
-                echo"<table border='widefat'>
+                echo "<form method='post' action='tentamenrooster.php'>;
+                <table border='widefat'>
                     <tr>
-                        <th>academie</th><th>surveillant</th><th>datum</th><th>begintijd</th><th>eindtijd</th><th>lokaal</th><th>omschrijving</th>
+                        <th>academie</th>
+                        <th>surveillant</th>
+                        <th>datum</th>
+                        <th>begintijd</th>
+                        <th>eindtijd</th>
+                        <th>lokaal</th>
+                        <th>omschrijving</th>
                     </tr>";
                 while($row = $result->fetch_assoc()){
                     echo"<tr>
-                        <td>".$row['academie']."</td><td></td><td>".$row['datum']."</td><td>".$row['begin_tijd']."</td><td>".$row['eind_tijd']."</td><td>".$row['lokaal']."</td><td>".$row['omschrijving']."</td>
+                        <td>".$row['academie']."</td>
+                        <td>
+                        <select name=".$row['toets_ID'].">
+                            ";
+                    if($surveillantenresult->num_rows > 0)
+                    {
+                        while($survrow = $surveillantenresult->fetch_assoc()){
+                            echo "<option value=".$survrow['pers_nummer'].">".$survrow['Voornaam']." ".$survrow['Tussenvoegsel']." ".$survrow['Achternaam']."</option>";
+                        }
+                    }
+                        echo "
+                        </select>
+                        </td>
+                        <td>".$row['datum']."</td>
+                        <td>".$row['begin_tijd']."</td>
+                        <td>".$row['eind_tijd']."</td>
+                        <td>".$row['lokaal']."</td>
+                        <td>".$row['omschrijving']."</td>
                     </tr>";
                 }
                 echo"</table>";
@@ -40,6 +75,12 @@
             } else {
                 echo"<h2>geen toetsen deze week</h2>";
             }
+
+    function tijdNaarDagdeel($begintijd,$eindtijd)
+    {
+
+    }
+
         ?>
     </body>
 </html>
