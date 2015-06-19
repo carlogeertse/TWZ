211176ffef81689f3f68f9b85bee468c7e5d4147
gamedevil12 <jonk0017@hz.nl>
2015-06-19 08:38:58 +0200

    done

---
+++ academieToevoegen.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<?php include("connectDB.php"); ?>
+<html>
+<head>
+    <title>
+        academie
+    </title>
+    <link rel="stylesheet" type="text/css" href="Style.css">
+</head>
+
+<body style="background-image: url(src/download.jpg); position: relative; background-size: cover;">
+<?php include("Menu.php"); ?>
+<?php
+include("Menu.php");
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $sql = "INSERT INTO academie(naam)
+                            VALUES(?)";
+
+    $stmt = $conn->prepare($sql);
+    $academie = $_POST['Academie'];
+    $stmt->bind_param("s", $academie);
+    $stmt->execute();
+    echo "<h3>Surveillant toegevoegd!</h3>";
+}
+
+?>
+
+<h1>
+    <b>
+        Invoeren Academie
+    </b>
+</h1>
+
+<table>
+    <form method="post" action="academieToevoegen.php">
+        <tr id="spaceUnder">
+            <td>
+                Academie:
+            </td>
+            <td>
+                <input type="text" name="Academie" required>
+            </td>
+        </tr>
+    </form>
+</table>
+
+</body>
+</html>
--- tentRooster.php
+++ tentRooster.php
@@ -1,31 +1,52 @@
 <!DOCTYPE html>
-<?php include ("connectDB.php"); ?>
+<?php include("connectDB.php"); ?>
 <html>
-    <head>
-        <title>
-            Rooster van de tentamens
-        </title>
-        <link rel = "stylesheet" type = "text/css" href = "Style.css">
-    </head>
+<head>
+    <title>
+        Rooster van de tentamens
+    </title>
+    <link rel="stylesheet" type="text/css" href="Style.css">
+</head>
 
-    <body style = "background-image: url(src/download.jpg); position: relative; background-size: cover;">
-    <?php include("Menu.php"); ?>
+<body style="background-image: url(src/download.jpg); position: relative; background-size: cover;">
+<?php include("Menu.php"); ?>
 
-    <h1>
-        <b>
-            Tentamenrooster
-        </b>
-    </h1>
+<h1>
+    <b>
+        Tentamenrooster
+    </b>
+</h1>
 
-    <h3> Kies een week </h3>
+<h3> Kies een week </h3>
 
-    <form action="tentamenrooster.php" method="POST" div="weekselectform">
-        Selecteer een week:
-        <input type="week" name="week" class="picker">
-        <input type="submit" class="submit"/>
-    </form>
-    <script src="js/legeWeekCheck.js"></script>
+<form action="tentamenrooster.php" method="POST" div="weekselectform">
+    Selecteer een week:
+    <input type="week" name="week" class="picker">
+    <input type="submit" class="submit"/>
+</form>
+<script src="js/legeWeekCheck.js"></script>
+<?php
+$sql = "SELECT T.toets_ID, T.pers_nummer, T.datum, T.begin_tijd, T.eind_tijd, T.lokaal, T.omschrijving, A.naam AS academie
+          FROM tentamen AS T, academie as A
+          WHERE A.academie_ID = T.academie_ID";
 
-    </body>
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    echo "<table border='widefat'>
+                    <tr>
+                        <th>academie</th><th>surveillant</th><th>datum</th><th>begintijd</th><th>eindtijd</th><th>lokaal</th><th>omschrijving</th>
+                    </tr>";
+    while ($row = $result->fetch_assoc()) {
+        echo "<tr>
+                 <td>" . $row['academie'] . "</td><td></td><td>" . $row['datum'] . "</td><td>" . $row['begin_tijd'] . "</td><td>" . $row['eind_tijd'] . "</td><td>" . $row['lokaal'] . "</td><td>" . $row['omschrijving'] . "</td>
+             </tr>";
+    }
+    echo "</table>";
+}
+
+?>
+
+</body>
 </html>
 
